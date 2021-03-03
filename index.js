22efc585e6d641aa9afd01f24cbb17063eeb9dfb
tyler-Github <51767087+tyler-Github@users.noreply.github.com>
2021-03-03 10:57:04 +0000

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const emailForm = document.querySelector("#emailForm");
 
 const toast = document.querySelector(".toast");
 
-const baseURL = "https://innshare.herokuapp.com";
+const baseURL = "http://cloudware.unaux.com";
 const uploadURL = `${baseURL}/api/files`;
 const emailURL = `${baseURL}/api/files/send`;
 
